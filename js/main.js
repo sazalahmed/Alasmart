5a887d7c83ce63be0ea3a5d2f578ba56a60502d8
freelancersazal <freelancersazal@gmail.com>
2023-06-02 16:57:09 +0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,126 @@
+$(function () {
+
+    "use strict";
+
+    //======MENU FIX========
+    if ($('.main_menu').offset() != undefined) {
+        $(window).bind('scroll', function () {
+            if ($(window).scrollTop() > 40) {
+                $('.main_menu').addClass('menu_fix');
+            } else {
+                $('.main_menu').removeClass('menu_fix');
+            }
+        });
+    }
+
+    //=======COUNTDOWN======   
+    var d = new Date(),
+        countUpDate = new Date();
+    d.setDate(d.getDate() + 365);
+
+    simplyCountdown('.simply-countdown-one', {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        enableUtc: true
+    });
+
+
+    //======NICE SELECT========
+    $('.select_js').niceSelect();
+
+
+    //=========main.js=========   
+    $('.counter').countUp();
+
+
+    //=======TESTI SLIDER======
+    $('.category_slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="far fa-long-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-long-arrow-left prevArrow"></i>',
+
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
+
+
+    //==========ISOTOPE==============
+    var $grid = $('.grid').isotope({});
+
+    $('.gallery_filter').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+
+    //active class
+    $('.gallery_filter button').on("click", function (event) {
+
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        event.preventDefault();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
